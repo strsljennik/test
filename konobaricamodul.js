1d08b1f30ae16c5a3ed6226f162cde925af3fc51
amateur5 <122245513+amateur5@users.noreply.github.com>
2024-11-16 17:01:44 +0100

    Update konobaricamodul.js

--- konobaricamodul.js
+++ konobaricamodul.js
@@ -4,7 +4,7 @@ module.exports = (io) => {
     io.on('connection', (socket) => {
         // Kada novi gost uđe, pošaljemo pozdravnu poruku od Konobarice
         socket.on('new_guest', () => {
-            const greetingMessage = `Dobro nam došli, osećajte se kao kod kuće, i budite nam raspoloženi! Sada će vam vaša Konobarica posluziti kaficu ☕, 
+            const greetingMessage = `Dobro nam došli, osećajte se kao kod kuće, i budite raspoloženi! Sada će vam vaša Konobarica posluziti kaficu ☕, 
                                                         a naši DJ-evi će se pobrinuti da vam ispune muzičke želje.`;
 
             // Emituj poruku sa klasom 'konobarica' kako bi imala stil
