8e7d7e615eb81630eb6727869fdc60bc0febcbb4
amateur5 <122245513+amateur5@users.noreply.github.com>
2024-11-16 18:30:46 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,88 +1,63 @@
-// server.js
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const { connectDB } = require('./mongo');
-const { register, login } = require('./prijava'); // Uvozimo register i login funkcije
-const { setupSocketEvents } = require('./banModule'); // Uvoz setupSocketEvents funkcije za banovanje
+const { register, login } = require('./prijava');  // Uvozimo register i login funkcije
 require('dotenv').config();
+const { setupSocketEvents } = require('./banModule'); // Putanja do banmodule.js
+const konobarica = require('./konobaricamodul');
+
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
-require('./konobaricamodul')(io);
+const io = socketIo(server); // Definišemo io ovde
 
-// Inicijalizacija objekata za goste i postavke korisnika
-const guests = {}; // Ovdje čuvamo sve goste sa njihovim socket ID-evima
-const userSettings = {}; // Ovdje čuvamo postavke svakog korisnika, kao što su boja i nadimak
+// Poveži se sa bazom podataka
+connectDB();
 
-// Povezivanje sa bazom podataka
-connectDB().catch((err) => {
-    console.error("Greška prilikom povezivanja sa bazom podataka:", err);
-    process.exit(1); // Zaustavi server ako se ne može povezati
-});
-
-// Middleware za parsiranje JSON-a i statičkih fajlova
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
 // Ruta za registraciju korisnika
-app.post('/register', (req, res) => register(req, res, io));
+app.post('/register', (req, res) => register(req, res, io));  // Prosleđujemo io kao argument
 
 // Ruta za prijavu korisnika
-app.post('/login', (req, res) => login(req, res, io));
+app.post('/login', (req, res) => login(req, res, io));  // Prosleđujemo io kao argument
 
 // Endpoint za glavnu stranicu
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-// Socket.io događaji
-setupSocketEvents(io);
+// Konekcija sa socket-om za rad sa događajima na strani klijenta
+let guests = {};
+let assignedNumbers = new Set();
+let connectedIps = [];
 
 io.on('connection', (socket) => {
     console.log('Novi gost je povezan sa socket ID:', socket.id);
 
     const guestId = socket.id;
+    const ip = socket.request.connection.remoteAddress;
+    console.log(`Gost sa IP adresom ${ip} se povezao.`);
 
-    // Generisanje korisničkog imena
-    socket.username = socket.handshake.query.username || `Gost-${generateUniqueNumber()}`;
-    console.log(`${socket.username} se povezao.`);
+    if (!connectedIps.includes(ip)) {
+        connectedIps.push(ip);
+    }
 
-    // Dodajemo gosta u listu gostiju i postavke
-    guests[guestId] = socket.username;
-    userSettings[guestId] = { nickname: socket.username, color: '#808080' }; // Postavke sa početnim vrednostima
+    const uniqueNumber = generateUniqueNumber();
+    const nickname = `Gost-${uniqueNumber}`;
+    guests[guestId] = nickname;
+    console.log(`${nickname} se povezao.`);
 
-    // Emitovanje događaja za povezivanje novog gosta
-    socket.broadcast.emit('newGuest', socket.username);
-    io.emit('updateGuestList', Object.values(guests));
+    socket.broadcast.emit('newGuest', nickname);
+    emitUpdatedGuestList(); // Emituj prvobitnu listu gostiju
 
-    // Pošaljemo postavke svim korisnicima na konekciju
-    socket.emit('updateSettings', userSettings);
-
-    // Dodajemo funkcionalnost za setovanje korisničkog imena i povezivanje sa sessionId
-    handleSocketConnection(socket);
-
-    // Rukovanje događajem kada se korisnik prijavi (menja nickname)
     socket.on('userLoggedIn', (username) => {
-        if (username) {
-            console.log(`Korisnik ${guests[guestId]} promenjen na ${username}`);
-            guests[guestId] = username;
-            userSettings[guestId].nickname = username; // Ažuriraj nickname u postavkama
-            io.emit('updateGuestList', Object.values(guests));
-            io.emit('updateSettings', userSettings); // Pošaljemo sve postavke
-        }
+        guests[guestId] = username;
+        emitUpdatedGuestList(); // Emituj ažuriranu listu gostiju
     });
 
-    // Rukovanje promenom boje za korisnika
-    socket.on('changeColor', (color) => {
-        if (color) {
-            userSettings[guestId].color = color; // Ažuriraj boju korisnika
-            io.emit('updateSettings', userSettings); // Pošaljemo sve postavke
-        }
-    });
-
-    // Rukovanje chat porukama
-    socket.on('chatMessage', async (msgData) => {
+    socket.on('chatMessage', (msgData) => {
         const time = new Date().toLocaleTimeString();
         const messageToSend = {
             text: msgData.text,
@@ -92,50 +67,40 @@ io.on('connection', (socket) => {
             nickname: guests[guestId],
             time: time
         };
-
-        // Čuvanje poruke u bazi
-        await saveMessage(messageToSend);
-
-        // Emitovanje poruke svim korisnicima
         io.emit('chatMessage', messageToSend);
     });
 
-    // Rukovanje odjavom korisnika
     socket.on('disconnect', () => {
         console.log(`${guests[guestId]} se odjavio.`);
+        assignedNumbers.delete(parseInt(guests[guestId].split('-')[1], 10));
         delete guests[guestId];
-        delete userSettings[guestId]; // Uklanjanje postavki korisnika pri disconnectu
-        io.emit('updateGuestList', Object.values(guests));
-        io.emit('updateSettings', userSettings); // Ažuriraj sve korisnike sa novim postavkama
+        connectedIps = connectedIps.filter((userIp) => userIp !== ip);
+        emitUpdatedGuestList(); // Emituj ažuriranu listu gostiju
     });
 });
+
+// Funkcija za emitovanje ažurirane liste korisnika
+function emitUpdatedGuestList() {
+    const updatedGuestList = Object.values(guests);
+    io.emit('updateGuestList', updatedGuestList); // Emituj novu listu korisnika
+}
 
 // Generisanje jedinstvenog broja za goste
 function generateUniqueNumber() {
     let number;
     do {
         number = Math.floor(Math.random() * 8889) + 1111;
-    } while (Object.values(guests).includes(`Gost-${number}`));
+    } while (assignedNumbers.has(number));
+    assignedNumbers.add(number);
     return number;
 }
 
-// Funkcija za povezivanje korisničkog imena sa sessionId
-const handleSocketConnection = (socket) => {
-  socket.on('setUsername', async (nickname, color) => {
-    try {
-      const existingUser = await getUserByNickname(nickname);
-      if (!existingUser) {
-        const sessionId = socket.id; // Koristimo socket.id kao sessionId
-        await saveUser(nickname, color, sessionId); // Sačuvaj korisnika u bazi
-      }
-      socket.emit('usernameSet', nickname); // Pošaljemo korisniku potvrdu
-    } catch (err) {
-      console.error(err);
-    }
-  });
-};
+// Postavljanje socket događaja iz banmodule.js
+setupSocketEvents(io);
 
-// Pokretanje servera
+konobarica(io);
+
+// Slušanje na određenom portu
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, '0.0.0.0', () => {
     console.log(`Server je pokrenut na portu ${PORT}`);
