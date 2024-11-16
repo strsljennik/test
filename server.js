0da9f15bfb4eb887aabea98de3bf27cb165531c7
amateur5 <122245513+amateur5@users.noreply.github.com>
2024-11-16 17:11:16 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const socketIo = require('socket.io');
 const { connectDB } = require('./mongo');
 const { register, login } = require('./prijava'); // Uvozimo register i login funkcije
 const { setupSocketEvents } = require('./banModule'); // Uvoz setupSocketEvents funkcije za banovanje
-require('dotenv').config();7
+const io = socketIo(server); 
+require('dotenv').config();
 require('./konobaricamodul')(io);
 
 
