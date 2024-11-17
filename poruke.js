27948d3da2a6f558542f8105a06ee1b6b636a385
amateur5 <122245513+amateur5@users.noreply.github.com>
2024-11-17 10:28:22 +0100

    Update poruke.js

--- poruke.js
+++ poruke.js
@@ -1,38 +1,41 @@
-// poruke.js
+const fs = require('fs');
 
-let trenutniKorisnici = {};  // Čuvanje podataka samo za trenutne korisnike
+// Putanja do JSON fajla
+const filePath = './korisnici.json';
 
-// Funkcija za dodavanje korisnika i njihovih podataka
-const dodajKorisnika = (guestId, podaci) => {
-  trenutniKorisnici[guestId] = podaci;
-};
+// Funkcija za učitavanje podataka iz JSON fajla
+function loadGuests() {
+    try {
+        const data = fs.readFileSync(filePath, 'utf8');
+        return JSON.parse(data);
+    } catch (err) {
+        return {}; // Ako fajl ne postoji, vrati prazan objekat
+    }
+}
 
-// Funkcija za dobijanje podataka o korisniku
-const getKorisnikaPodaci = (guestId) => {
-  return trenutniKorisnici[guestId];
-};
+// Funkcija za upisivanje podataka u JSON fajl
+function saveGuests(guests) {
+    fs.writeFileSync(filePath, JSON.stringify(guests, null, 2));
+}
 
-// Funkcija za brisanje podataka korisnika
-const ukloniKorisnika = (guestId) => {
-  delete trenutniKorisnici[guestId];
-};
+// Funkcija za dodavanje korisnika (gosta) u fajl
+function addGuest(nick, color, number) {
+    const guests = loadGuests();
+    
+    // Dodaj novog gosta
+    guests[nick] = { color, number };
 
-// Funkcija za dobijanje svih podataka (ako je potrebno)
-const getSviKorisniciPodaci = () => {
-  return trenutniKorisnici;
-};
+    // Spasi promene u fajl
+    saveGuests(guests);
+}
 
-// Početna dodela podataka korisniku
-const pocetniPodaci = () => ({ 
-  boja: 'default', 
-  nik: 'Guest', 
-  broj: Math.floor(Math.random() * 1000) 
-});
+// Funkcija za dobijanje podataka o svim korisnicima
+function getGuests() {
+    return loadGuests();
+}
 
+// Izvoz svih funkcija kao API za korišćenje u drugim delovima aplikacije
 module.exports = {
-  dodajKorisnika,
-  getKorisnikaPodaci,
-  ukloniKorisnika,
-  getSviKorisniciPodaci,
-  pocetniPodaci,
+    addGuest,
+    getGuests
 };
