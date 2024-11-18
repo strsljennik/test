1bdf364e8b159a863f0736ebc0bcce65e809a04b
amateur5 <122245513+amateur5@users.noreply.github.com>
2024-11-18 06:59:02 +0100

    Update userData.js

--- userData.js
+++ userData.js
@@ -6,31 +6,44 @@ const loadUserData = () => {
     try {
         return JSON.parse(fs.readFileSync(path, 'utf-8'));
     } catch (err) {
-        console.log('Fajl nije pronađen, kreiraćemo novi.');
         return [];  // Ako fajl ne postoji, vrati prazan niz
     }
 };
 
-// Sačuvaj nove podatke u users.json
-const saveUserData = (username, color) => {
-    const users = loadUserData();  // Učitaj postojeće podatke
-    console.log('Trenutni podaci:', users);  // Prikazivanje trenutnog stanja
-    users.push({ username, color });  // Dodaj novog korisnika
-    console.log('Dodati korisnik:', { username, color });
-    fs.writeFileSync(path, JSON.stringify(users, null, 2));  // Upisuj u fajl
-};
-
-// Ažuriraj boju korisnika
-const updateUserColor = (username, newColor) => {
+// Sačuvaj podatke u users.json
+const saveUserData = (username, id) => {
     const users = loadUserData();
-    const userIndex = users.findIndex(u => u.username === username);
-    if (userIndex !== -1) {
-        users[userIndex].color = newColor;
-        fs.writeFileSync(path, JSON.stringify(users, null, 2));  // Upisuj promenjenu boju
-        console.log(`Boja korisnika ${username} ažurirana na ${newColor}`);
-    } else {
-        console.log(`Korisnik ${username} nije pronađen.`);
+    
+    // Proveri da li već postoji korisnik sa istim ID-om, pa ga ne dodaj ponovo
+    if (!users.some(user => user.id === id)) {
+        users.push({ username, id });
+        fs.writeFileSync(path, JSON.stringify(users, null, 2));  // Upisuj u fajl
     }
 };
 
-module.exports = { loadUserData, saveUserData, updateUserColor };
+// Pronađi korisnika po ID-u
+const getUserById = (id) => {
+    const users = loadUserData();
+    return users.find(user => user.id === id);  // Vrati korisnika sa datim ID-om
+};
+
+// Korišćenje funkcija za dodavanje i učitavanje korisnika
+const handleUserJoin = (id) => {
+    const user = getUserById(id);
+
+    if (user) {
+        console.log(`Korisnik sa ID-om ${id} vraća se sa nikom ${user.username}`);
+    } else {
+        // Ako je korisnik nov, dodeli mu novi ID i ime
+        const username = `gost-${id}`;  // Primer kako možeš dodeliti naziv za gosta
+        saveUserData(username, id);
+        console.log(`Korisnik sa ID-om ${id} ulazi kao ${username}`);
+    }
+};
+
+// Primer upisa korisnika i učitavanja pri ulasku
+handleUserJoin('guest-5555');  // Ovo će sačuvati korisnika sa ID 'guest-5555'
+
+// Ovo simulira da server ponovo pokreće i učitava korisnike
+handleUserJoin('guest-5555');  // Ovo će učitati prethodni podaci za korisnika
+handleUserJoin('guest-7777');  // Novi korisnik, biće dodeljen nik 'gost-7777'
