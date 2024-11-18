43985dcd464a1b7a8cf78ee2f2cdb77bdb40f16b
amateur5 <122245513+amateur5@users.noreply.github.com>
2024-11-18 12:18:54 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const socketIo = require('socket.io');
 const { connectDB } = require('./mongo');
 const { register, login } = require('./prijava');  // Uvozimo register i login funkcije
 require('dotenv').config();
-const { setupSocketEvents } = require('./banModule'); // Putanja do banmodule.js
-const konobarica = require('./konobaricamodul');
 
 const app = express();
 const server = http.createServer(app);
@@ -29,31 +27,34 @@ app.get('/', (req, res) => {
 });
 
 // Konekcija sa socket-om za rad sa događajima na strani klijenta
-let guests = {};  // Svi gosti sa njihovim podacima
-let assignedNumbers = new Set();  // Set za dodeljene brojeve
-let connectedIps = [];  // Lista povezanih IP adresa
+let guests = {};
+let assignedNumbers = new Set();
+let connectedIps = [];
 
 io.on('connection', (socket) => {
     console.log('Novi gost je povezan sa socket ID:', socket.id);
 
-    // Generisanje jedinstvenog broja za svakog gosta
+    const guestId = socket.id;
+    const ip = socket.request.connection.remoteAddress;
+    console.log(`Gost sa IP adresom ${ip} se povezao.`);
+
+    if (!connectedIps.includes(ip)) {
+        connectedIps.push(ip);
+    }
+
     const uniqueNumber = generateUniqueNumber();
-    const guestId = socket.id;  // Koristimo socket.id kao guestId
     const nickname = `Gost-${uniqueNumber}`;
-    
-    guests[guestId] = nickname;  // Dodajemo novog gosta sa njegovim nickom
+    guests[guestId] = nickname;
     console.log(`${nickname} se povezao.`);
 
     socket.broadcast.emit('newGuest', nickname);
-    emitUpdatedGuestList();  // Emituj listu gostiju
+    io.emit('updateGuestList', Object.values(guests));
 
-    // Kada se korisnik prijavi sa svojim username-om
     socket.on('userLoggedIn', (username) => {
-        guests[guestId] = username;  // Ažuriramo nickname
-        emitUpdatedGuestList();  // Emituj ažuriranu listu gostiju
+        guests[guestId] = username;
+        io.emit('updateGuestList', Object.values(guests));
     });
 
-    // Handling chat messages
     socket.on('chatMessage', (msgData) => {
         const time = new Date().toLocaleTimeString();
         const messageToSend = {
@@ -67,34 +68,24 @@ io.on('connection', (socket) => {
         io.emit('chatMessage', messageToSend);
     });
 
-    // Kada se korisnik odjavi
     socket.on('disconnect', () => {
         console.log(`${guests[guestId]} se odjavio.`);
-        assignedNumbers.delete(parseInt(guests[guestId].split('-')[1], 10));  // Oslobađanje broja
-        delete guests[guestId];  // Brisanje gosta iz liste
-        emitUpdatedGuestList();  // Emituj ažuriranu listu gostiju
+        assignedNumbers.delete(parseInt(guests[guestId].split('-')[1], 10));
+        delete guests[guestId];
+        connectedIps = connectedIps.filter((userIp) => userIp !== ip);
+        io.emit('updateGuestList', Object.values(guests));
     });
-
-// Funkcija za emitovanje ažurirane liste korisnika
-function emitUpdatedGuestList() {
-    const updatedGuestList = Object.values(guests);
-    io.emit('updateGuestList', updatedGuestList);  // Emituj novu listu korisnika
-}
+});
 
 // Generisanje jedinstvenog broja za goste
 function generateUniqueNumber() {
     let number;
     do {
         number = Math.floor(Math.random() * 8889) + 1111;
-    } while (assignedNumbers.has(number));  // Proveri da li je broj već dodeljen
+    } while (assignedNumbers.has(number));
     assignedNumbers.add(number);
     return number;
 }
-
-// Postavljanje socket događaja iz banmodule.js
-setupSocketEvents(io);
-
-konobarica(io);
 
 // Slušanje na određenom portu
 const PORT = process.env.PORT || 3000;
