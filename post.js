3452c86437a054f6f20ae772639a41c48d8e992e
amateur5 <122245513+amateur5@users.noreply.github.com>
2024-11-18 13:40:13 +0100

    Update post.js

--- post.js
+++ post.js
@@ -1,56 +1,51 @@
-// post.js
-import { Client } from 'pg';
+// chat.js
+const { Client } = require('pg'); // Uvezi pg
+const pgp = require('pg-promise')(); // Uvezi pg-promise
 
-// Kreiraj i poveži se sa bazom
+const connectionString = 'postgres://radio_galaksija_user:8bhm0m6LkEbfUSN916ygt8szcMrvgeNu@dpg-cstia1tumphs73fp4c00-a/radio_galaksija';
+
+// Kreiraj klijent za PostgreSQL
 const client = new Client({
-  connectionString: process.env.POSTGRE_URI,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+    connectionString: connectionString,
 });
 
 client.connect()
-  .then(() => {
-    console.log('Povezan sa PostgreSQL bazom');
-  })
-  .catch(err => {
-    console.error('Greška pri povezivanju sa bazom:', err.stack);
-  });
+    .then(() => console.log('Connected to the database'))
+    .catch(err => console.error('Connection error', err.stack));
 
-// Funkcija za dodelu boje na osnovu ID-a
-export function getColorById(id) {
-  const idStr = String(id);
-  const colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange'];
-  const colorIndex = parseInt(idStr.split('-')[1]) % colors.length;
-  return colors[colorIndex];
-}
+const db = pgp(connectionString);
 
-// Funkcija za dodavanje korisnika u bazu
-export function addUserToDatabase(username, color, ipAddress) {
-  client.query('SELECT * FROM users WHERE username = $1', [username], (err, res) => {
-    if (err) {
-      console.error('Greška pri upitu:', err);
-    } else {
-      if (res.rows.length === 0) {
-        // Dodavanje novog korisnika u bazu
-        client.query('INSERT INTO users (username, color, ip_address) VALUES ($1, $2, $3)', [username, color, ipAddress], (err) => {
-          if (err) {
-            console.error('Greška pri upisu korisnika:', err);
-          } else {
-            console.log(`Korisnik ${username} sa bojom ${color} i IP adresom ${ipAddress} je sačuvan.`);
-          }
-        });
-      }
+// Funkcija za čuvanje korisnika u bazi
+const saveUser = async (nickname, number, color) => {
+    try {
+        await db.none('INSERT INTO users(nickname, number, color) VALUES($1, $2, $3)', [nickname, number, color]);
+    } catch (error) {
+        console.error('Error saving user:', error);
     }
-  });
-}
+};
 
-// Funkcija za dobijanje svih korisnika
-export function getAllUsers() {
-  return client.query('SELECT * FROM users WHERE banned = false');
-}
+// Funkcija za dohvat svih korisnika iz baze
+const getUsers = async () => {
+    try {
+        const users = await db.any('SELECT * FROM users');
+        return users;
+    } catch (error) {
+        console.error('Error retrieving users:', error);
+    }
+};
 
-// Funkcija za banovanje korisnika
-export function banUser(username) {
-  return client.query('UPDATE users SET banned = true WHERE username = $1', [username]);
-}
+// Izvoz funkcija
+module.exports = {
+    saveUser,
+    getUsers,
+};
+
+// Ako želiš da se svi korisnici učitaju na serveru nakon restarta:
+const loadUsersOnStart = async () => {
+    const users = await getUsers();
+    // Ovde možeš implementirati logiku da nastaviš chat sa učitanim korisnicima
+    console.log('Users loaded:', users);
+};
+
+// Učitaj korisnike prilikom starta
+loadUsersOnStart();
