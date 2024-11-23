341f3b44997b768f387c253f7a20c8aa784c78fa
amateur5 <122245513+amateur5@users.noreply.github.com>
2024-11-23 13:40:14 +0100

    Update formatiranje.js

--- public/formatiranje.js
+++ public/formatiranje.js
@@ -73,8 +73,9 @@ function addGuestStyles(guestElement, guestId) {
     colorPickerButton.classList.add('colorPicker');
      guestsData[guestId] = { color: 'currentColor', isBold: true, isItalic: true };
      colorPickerButton.addEventListener('input', function() {
-        guestElement.style.color = this.value;
-        guestsData[guestId].color = this.value; // Ažuriraj boju u objektu
+      const guestData = guestsData[guestId] || { color: '#808080' };  // Default siva boja
+      newGuest.style.color = guestData.color;
+
     });
 
     const boldButton = document.createElement('button');
