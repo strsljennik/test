429271e7a50bbcf5f27b9984a6b578f7ae2826d3
amateur5 <122245513+amateur5@users.noreply.github.com>
2024-11-23 10:54:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const http = require('http');
 const socketIo = require('socket.io');
 const { connectDB } = require('./mongo');
 const { register, login } = require('./prijava');
-const { initializeStorage, saveGuestData, loadGuestData } = require('./storage');
 require('dotenv').config();
 require('./ping');
 
@@ -12,7 +11,6 @@ const server = http.createServer(app);
 const io = socketIo(server);
 
 connectDB();
-initializeStorage(); // Inicijalizuj storage pre nego što nastavimo sa serverom
 
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
@@ -31,14 +29,10 @@ let assignedNumbers = new Set();
 
 io.on('connection', (socket) => {
     const uniqueNumber = generateUniqueNumber();
-    const username = "register user";  // Ako je 'register user' vrednost
     const nickname = `Gost-${uniqueNumber}`;
     guests[socket.id] = nickname;
-    
 
-    saveGuestData(socket.id, nickname); // Spasi podatke gosta u storage
     console.log(`${nickname} se povezao.`);
-
     socket.broadcast.emit('newGuest', nickname);
     io.emit('updateGuestList', Object.values(guests));
 
@@ -47,10 +41,9 @@ io.on('connection', (socket) => {
             guests[socket.id] = `${username} (Admin)`;
             console.log(`${username} je autentifikovan kao admin.`);
         } else {
-            guests[socket.id] = socketid;
-            console.log(`${socketid} se prijavio kao gost.`);
+            guests[socket.id] = username;
+            console.log(`${username} se prijavio kao gost.`);
         }
-        await saveGuestData(socket.id, guests[socket.id]); // Ažuriraj podatke gosta u storage
         io.emit('updateGuestList', Object.values(guests));
     });
 
@@ -67,10 +60,9 @@ io.on('connection', (socket) => {
         io.emit('chatMessage', messageToSend);
     });
 
-    socket.on('disconnect', async () => {
+    socket.on('disconnect', () => {
         console.log(`${guests[socket.id]} se odjavio.`);
         assignedNumbers.delete(parseInt(guests[socket.id].split('-')[1], 10));
-        await saveGuestData(socket.id, null); // Obrisi podatke gosta kad se odjavi
         delete guests[socket.id];
         io.emit('updateGuestList', Object.values(guests));
     });
