06a31c432f313d7a894faf3b362bf7a7edf5bc4a
amateur5 <122245513+amateur5@users.noreply.github.com>
2024-12-11 11:13:06 +0100

    Update slikemodul.js

--- slikemodul.js
+++ slikemodul.js
@@ -1,22 +1,27 @@
-
 let io;
-let newImage = [];  
+let newImage = [];
+let userImages = {}; // Mapa korisničkih slika
 
 // Funkcija za setovanje io objekta
 function setSocket(serverIo) {
     io = serverIo;
 
     io.on('connection', (socket) => {
+        userImages[socket.id] = []; // Inicializacija korisničkih slika
+
         socket.emit('initial-images', newImage);
 
         socket.on('add-image', (imageSource, position, dimensions) => {
             if (!imageSource || !position || !dimensions) return;
 
-            newImage.push({
+            const image = {
                 imageUrl: imageSource,
                 position: position,
                 dimensions: dimensions
-            });
+            };
+
+            newImage.push(image);
+            userImages[socket.id].push(image);
 
             io.emit('display-image', {
                 imageUrl: imageSource,
@@ -38,8 +43,26 @@ function setSocket(serverIo) {
             const index = newImage.findIndex(img => img.imageUrl === imageUrl);
             if (index !== -1) {
                 newImage.splice(index, 1);
+                userImages[socket.id] = userImages[socket.id].filter(img => img.imageUrl !== imageUrl);
             }
             io.emit('update-images', newImage);
+        });
+
+        socket.on('disconnect', () => {
+            // Ukloni slike korisnika kad se socket diskonektuje
+            newImage = newImage.filter(img => !userImages[socket.id].includes(img));
+            delete userImages[socket.id];
+            io.emit('update-images', newImage);
+        });
+
+        socket.on('delete-all', (password) => {
+            if (password === 'your_password') { // Provera lozinke
+                newImage = [];
+                userImages = {};
+                io.emit('update-images', newImage); // Obavesti sve klijente
+            } else {
+                socket.emit('error', 'Pogrešna lozinka!');
+            }
         });
     });
 }
