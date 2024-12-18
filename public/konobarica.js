e2ce8d3826fbf6b638805c1351dc6d7cad826037
amateur5 <122245513+amateur5@users.noreply.github.com>
2024-12-18 03:05:01 +0100

    Update konobarica.js

--- public/konobarica.js
+++ public/konobarica.js
@@ -24,8 +24,57 @@ socket.on('message', (data) => {
 });
 
 // Dodavanje Konobarice u listu gostiju (ako je potrebno)
-const guestList = document.getElementById('guestList');
 const konobaricaItem = document.createElement('div');
 konobaricaItem.classList.add('guest-konobarica');
 konobaricaItem.innerHTML = 'Konobarica'; // Dodajemo samo tekst bez tagova
 guestList.appendChild(konobaricaItem);
+
+//  ZA MIXER DODATAK
+// Open modal on button click
+document.getElementById("mixer").onclick = function() {
+  document.getElementById("mixerModal").style.display = "block";
+}
+
+// Close modal when clicking the close button
+document.getElementById("kosModal").addEventListener("click", function() {
+  document.getElementById("mixerModal").style.display = "none";
+});
+
+
+// GOSTI MODAL 
+var modal = document.getElementById("gostimodal");
+var btn = document.getElementById("GBtn");
+var span = document.getElementsByClassName("close")[0];
+
+// Otvori modal kada klikneš na dugme GBtn
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// Zatvori modal kada klikneš na X
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Zatvori modal klikom izvan njega
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Funkcija za uvećanje fonta
+function increaseFontSize() {
+    var messageArea = document.getElementById("messageArea");
+    var currentSize = window.getComputedStyle(messageArea, null).getPropertyValue('font-size');
+    var newSize = parseInt(currentSize) + 2; // Povećaj veličinu fonta za 2px
+    messageArea.style.fontSize = newSize + "px";
+}
+
+// Funkcija za smanjenje fonta
+function decreaseFontSize() {
+    var messageArea = document.getElementById("messageArea");
+    var currentSize = window.getComputedStyle(messageArea, null).getPropertyValue('font-size');
+    var newSize = parseInt(currentSize) - 2; // Smanji veličinu fonta za 2px
+    messageArea.style.fontSize = newSize + "px";
+}
