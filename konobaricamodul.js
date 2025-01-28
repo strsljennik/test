41e59542904723ca33e578b53addcf4c46b0c473
amateur5 <122245513+amateur5@users.noreply.github.com>
2025-01-28 23:25:03 +0100

    Update konobaricamodul.js

--- konobaricamodul.js
+++ konobaricamodul.js
@@ -1,18 +1,36 @@
-// konobaricamodul.js
-
 module.exports = (io) => {
-    io.on('connection', (socket) => {
-        // Kada novi gost uđe, pošaljemo pozdravnu poruku od Konobarice
-        socket.on('new_guest', () => {
-            const greetingMessage = `Dobro nam došli, osećajte se kao kod kuće, i budite raspoloženi! Sada će vam vaša Konobarica posluziti kaficu ☕, 
-                                                        a naši DJ-evi će se pobrinuti da vam ispune muzičke želje.`;
+    let chatContainerPosition = { x: 0, y: 0 }; // Početna pozicija chat kontejnera
 
-            // Emituj poruku sa klasom 'konobarica' kako bi imala stil
+    io.on('connection', (socket) => {
+        console.log('A user connected: ' + socket.id);
+
+        // Kada novi gost uđe, šaljemo mu pozdravnu poruku od Konobarice
+        socket.on('new_guest', () => {
+            const greetingMessage = `Dobro nam došli, osećajte se kao kod kuće, i budite nam raspoloženi! Sada će vam vaša Konobarica posluziti kaficu ☕, 
+                                    a naši DJ-evi će se pobrinuti da vam ispune muzičke želje.`;
+
+            // Emitujemo pozdravnu poruku svima
             io.emit('message', { 
-                username: '<span class="konobarica">Konobarica</span>', 
+                username: '<span class="konabarica">Konobarica</span>', 
                 message: greetingMessage,
                 isSystemMessage: true // označavamo kao sistemsku poruku
             });
+
+            // Šaljemo trenutnu poziciju chat kontejnera novom korisniku
+            socket.emit('updateChatContainer', chatContainerPosition);
+        });
+
+        // Kada se korisnik poveže, šaljemo mu trenutnu poziciju chat kontejnera (u slučaju da se ne aktivira 'new_guest')
+        socket.emit('updateChatContainer', chatContainerPosition);
+
+        // Kada klijent pošalje poziciju chat kontejnera
+        socket.on('moveChatContainer', (data) => {
+            chatContainerPosition = data; // Ažuriraj poziciju chat kontejnera
+            io.emit('updateChatContainer', chatContainerPosition); // Emitujemo svima
+        });
+
+        socket.on('disconnect', () => {
+            console.log('User disconnected: ' + socket.id);
         });
     });
 };
