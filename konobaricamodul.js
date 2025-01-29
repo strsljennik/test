fa7cf7168d286bdbce1fcf0e2804c60a21e2b1fc
amateur5 <122245513+amateur5@users.noreply.github.com>
2025-01-29 18:46:53 +0100

    Update konobaricamodul.js

--- konobaricamodul.js
+++ konobaricamodul.js
@@ -1,6 +1,5 @@
 module.exports = (io) => {
-    let chatContainerState = { x: 0, y: 0, width: 900, height: 600 };
-    let virtualGuests = []; // Lista virtuelnih gostiju
+   let chatContainerState = { x: 0, y: 0, width: 900, height: 600 };
 
     io.on('connection', (socket) => {
         console.log('A user connected: ' + socket.id);
@@ -34,20 +33,7 @@ module.exports = (io) => {
             }
         });
 
-        // Dodavanje virtuelnih gostiju
-        socket.on('addVirtualGuests', (guests) => {
-            virtualGuests = guests; // Ažuriraj listu virtuelnih gostiju
-            io.emit('updateVirtualGuests', virtualGuests); // Pošaljite svim korisnicima
-        });
-
-        // Uklanjanje virtuelnih gostiju
-        socket.on('removeVirtualGuests', () => {
-            virtualGuests = []; // Očisti listu virtuelnih gostiju
-            io.emit('updateVirtualGuests', virtualGuests); // Pošaljite svim korisnicima
-        });
-
-        // Pošaljite trenutnu listu virtuelnih gostiju kada se korisnik poveže
-        socket.emit('updateVirtualGuests', virtualGuests);
+        socket.emit('updateChatContainer', { ...chatContainerState });
 
         socket.on('disconnect', () => {
             console.log('User disconnected: ' + socket.id);
